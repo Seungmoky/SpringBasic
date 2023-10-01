0821ebffaf2f0fae6570e5c5b57a9f4be0c45ef5
Seungmoky <xkwksdnjs@gmail.com>
2023-10-01 23:33:08 +0900

    스프링 기본 1001 java만 이용해서 작성한 코드(spring X)

---
+++ core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,36 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import hello.core.member.MemberRepository;
+
+
+/**
+ * 구현객체가 구현객체에 의존하지 않고
+ * 인터페이스에 의존할 수 있도록 구현객체를 특정해준다.
+ * "생성자 주입"
+ */
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+}
